63dca9b7e362f3bafeb9e23ac059bf02872fdb07
sdegongzuo <sdegongzuo@163.com>
2024-10-07 10:36:36 +0800

    100

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,21 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+        let parts: Vec<&str> = s.split(',').collect();
+        if parts.len()!= 2  {
+            return Err(ParsePersonError::BadLen);
+        }
+        if parts[0].is_empty() {
+           return Err(ParsePersonError::NoName); 
+        }
+        let age_result = parts[1].parse::<usize>();
+        return age_result.map(|age| Person {
+            name: parts[0].to_string(),
+            age,
+        }).map_err(|err| ParsePersonError::ParseInt(err));
     }
 }
 
